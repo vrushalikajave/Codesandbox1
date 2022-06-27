99721b3d851fbc779af26f6a804fd65251702d04
Vrushali Kajave <99703905+vrushalikajave@users.noreply.github.com>
2022-06-27 06:13:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img src="https://tse1.mm.bing.net/th?id=OIP.RgUfG0aLfw7FxoCJXh4O_wHaE7&pid=Api&P=0&w=229&h=152" />
+      <h5>80% of Exam is always based on </h5>
+      <p> 1 lecture that you MISSED and 1 topic that you did't PREPARE !! </p>
+    </div>
+  );
+}
